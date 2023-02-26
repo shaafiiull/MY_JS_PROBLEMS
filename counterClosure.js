3a081f060ecb9a4a670d368fefb81fd581526a9c
shaafiiull <thisisshaafii@gmail.com>
2023-02-26 18:57:41 +0600

    implemented the same closure concept using self-invoking function

--- counterClosure.js
+++ counterClosure.js
@@ -1,4 +1,5 @@
-function add() {
+// example of calling the main function manually and then store the returned function to another variable
+/* function add() {
   let counter = 0;
   return function () {
     counter += 1;
@@ -9,4 +10,17 @@ function add() {
 const plus = add();
 console.log(plus());
 console.log(plus());
-console.log(plus());
+console.log(plus()); */
+
+// we can achieve the same result using the self-invoking function as it reduces the use of an extra variable
+const add = (function () {
+  let counter = 0;
+  return function () {
+    counter += 1;
+    return counter;
+  };
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
